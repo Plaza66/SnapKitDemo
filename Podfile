8ada90a4e3d90f4f70e37046aafa2a6b8ec11984
李延 <liyan>
2015-07-01 18:17:30 +0800

    添加工程文件

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, "8.0"
+use_frameworks!
+
+target 'SwiftDemo' do
+    pod 'SnapKit', '0.10.0'
+end
+
+target 'SwiftDemoTests' do
+    
+end
---
+++ SwiftDemo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SwiftDemo
+//
+//  Created by liyan on 15/7/1.
+//  Copyright (c) 2015年 liyan. All rights reserved.
+//
+
+import UIKit
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
