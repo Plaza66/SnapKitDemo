5a6d052dc9e9cd3a2650ad50d349a118b78379e0
李延 <liyan>
2015-07-03 21:19:29 +0800

    Bacony Labels

--- SwiftDemo/MASExampleLabelView.swift
+++ SwiftDemo/MASExampleLabelView.swift
@@ -7,11 +7,48 @@
 //
 
 import UIKit
-
 class MASExampleLabelView: UIView {
 
+    var kPadding: UIEdgeInsets =  UIEdgeInsetsMake(10, 10, 10, 10)
+
+    var shortLabel: UILabel!
+    var longLabel: UILabel!
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        self.shortLabel = UILabel()
+        self.shortLabel.numberOfLines = 1;
+        self.shortLabel.textColor = UIColor.purpleColor()
+        self.shortLabel.lineBreakMode = NSLineBreakMode.ByTruncatingTail
+        self.shortLabel.text = "Bacon";
+        self.addSubview(self.shortLabel)
+        
+        self.longLabel = UILabel()
+        self.longLabel.numberOfLines = 8
+        self.longLabel.textColor = UIColor.darkGrayColor()
+        self.longLabel.lineBreakMode = NSLineBreakMode.ByTruncatingTail
+        self.longLabel.text = "Bacon ipsum dolor sit amet spare ribs fatback kielbasa salami, tri-tip jowl pastrami flank short loin rump sirloin. Tenderloin frankfurter chicken biltong rump chuck filet mignon pork t-bone flank ham hock.";
+        self.addSubview(self.longLabel)
+
+        self.longLabel.snp_makeConstraints { (make) -> Void in
+            make.left.equalTo(self.snp_left).insets(kPadding)
+            make.top.equalTo(self.snp_top).insets(kPadding)
+        }
+        self.shortLabel.snp_makeConstraints { (make) -> Void in
+            make.centerY.equalTo(self.longLabel.snp_centerY)
+            make.right.equalTo(self.snp_right).insets(kPadding)
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        var width: CGFloat = CGRectGetMinX(self.shortLabel.frame) - kPadding.left;
+        width -= CGRectGetMinX(self.longLabel.frame);
+        self.longLabel.preferredMaxLayoutWidth = width;
+        
+        super.layoutSubviews()
         
     }
     
